754e5cdc0145bedc5b0eef2970c7f141389df935
yglee <fksfm1570@gmail.com>
2022-11-27 19:54:36 +0900

    spring security 추가

--- src/main/java/com/example/Sbb/Sbb/SecurityConfig.java
+++ src/main/java/com/example/Sbb/Sbb/SecurityConfig.java
@@ -20,6 +20,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         //모든 인증되지 않은 요청을 허락 => 로그인 하지 않아도 모든 페이지에 접근할 수 있음
+        //X-Frame-Options 헤더값을 sameorigin으로 설정하여
+        //frame에 포함된 페이지가 페이지를 제공하는 사이트와 동일한 경우에는 계속 사용할 수 있도록 함
         http.authorizeHttpRequests().antMatchers("/**").permitAll()
                 .and()
                 .csrf().ignoringAntMatchers("/h2-console/**")
