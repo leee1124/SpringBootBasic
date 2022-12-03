b0e40a91da4cd94d47dcb58a01125b97f783f821
yglee <fksfm1570@gmail.com>
2022-12-03 21:41:20 +0900

    중복 회원가입 에러

--- src/main/java/com/example/Sbb/Sbb/Controller/UserController.java
+++ src/main/java/com/example/Sbb/Sbb/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.Sbb.Sbb.Controller;
 import com.example.Sbb.Sbb.Form.UserCreateForm;
 import com.example.Sbb.Sbb.Service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,9 +35,17 @@ public class UserController {
             return "signup_form";
         }
 
-        userService.create(userCreateForm.getUsername(),
-                userCreateForm.getEmail(), userCreateForm.getPassword());
-
+        try {
+            userService.create(userCreateForm.getUsername(),
+                    userCreateForm.getEmail(), userCreateForm.getPassword());
+        }catch (DataIntegrityViolationException e){
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+        }catch (Exception e){
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
         return "redirect:/";
     }
 }
