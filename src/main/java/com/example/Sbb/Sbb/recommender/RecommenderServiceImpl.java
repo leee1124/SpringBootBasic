870435c7beb1f1c0583f72357b3b6a2e1809db56
leee1124 <fksfm1570@gmail.com>
2023-05-19 23:58:31 +0900

    일시저장

---
+++ src/main/java/com/example/Sbb/Sbb/recommender/RecommenderServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.Sbb.Sbb.recommender;
+
+import com.example.Sbb.Sbb.question.QuestionEntity;
+import com.example.Sbb.Sbb.user.SiteUserEntity;
+
+public class RecommenderServiceImpl implements RecommendService{
+    public SiteUserEntity getSiteUser(RecommenderEntity recommenderEntity){
+
+    }
+    public QuestionEntity getQuestion(SiteUserEntity siteUserEntity){
+
+    }
+}
